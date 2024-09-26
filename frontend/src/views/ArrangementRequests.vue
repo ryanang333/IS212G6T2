af9c946dca34e645df713fa597f84efb6b9f2224
KEugene0 <eugene.koh.2022@scis.smu.edu.sg>
2024-09-27 02:44:18 +0800

    Update ArrangementRequests.vue
    Included Paging
    Interval update commented for further development in future

--- frontend/src/views/ArrangementRequests.vue
+++ frontend/src/views/ArrangementRequests.vue
@@ -25,12 +25,14 @@
           <th class="border border-gray-300 p-2">Staff ID</th>
           <th class="border border-gray-300 p-2">Group ID</th>
           <th class="border border-gray-300 p-2">Request Date</th>
+          <th class="border border-gray-300 p-2">Request Time</th>
           <th class="border border-gray-300 p-2">Status</th>
+          <th class="border border-gray-300 p-2">Reason</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="request in arrangementRequests" :key="request._id">
-          <td>
+        <tr v-for="request in paginatedRequests" :key="request._id">
+          <td class="border border-gray-300 p-2">
             <div class="flex">
               <button class="bg-green-500 text-white rounded px-2 py-1 mr-1 hover:bg-green-600">Approve</button>
               <button class="bg-red-500 text-white rounded px-2 py-1 hover:bg-red-600">Reject</button>
@@ -40,10 +42,30 @@
           <td class="border border-gray-300 p-2">{{ request.staff_id }}</td>
           <td class="border border-gray-300 p-2">{{ request.group_id || "-" }}</td>
           <td class="border border-gray-300 p-2">{{ new Date(request.request_date).toLocaleString() }}</td>
+          <td class="border border-gray-300 p-2">{{ request.time }}</td>
           <td class="border border-gray-300 p-2">{{ request.status }}</td>
+          <td class="border border-gray-300 p-2">{{ request.reason }}</td>
         </tr>
       </tbody>
     </table>
+
+    <div class="flex justify-center space-x-2 mt-4">
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        class="bg-gray-300 rounded px-4 py-2 hover:bg-gray-400"
+      >
+        Previous
+      </button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="bg-gray-300 rounded px-4 py-2 hover:bg-gray-400"
+      >
+        Next
+      </button>
+    </div>
   </div>
 </template>
 
@@ -55,6 +77,8 @@ export default {
     return {
       manager_id: null,
       arrangementRequests: [],
+      currentPage: 1,
+      itemsPerPage: 10,
     };
   },
   methods: {
@@ -73,13 +97,40 @@ export default {
           alert('No requests found for the provided manager ID.');
         } else {
           this.arrangementRequests = response.data;
+          this.currentPage = 1;
         }
       } catch (error) {
         console.error('Error fetching arrangement requests:', error);
         this.arrangementRequests = [];
       }
     },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
   },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.arrangementRequests.length / this.itemsPerPage); 
+    },
+    paginatedRequests() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      return this.arrangementRequests.slice(start, start + this.itemsPerPage);
+    },
+  },
+  // mounted() {
+  //   this.fetchArrangementRequests();
+  //   this.interval = setInterval(this.fetchArrangementRequests, 1000);
+  // },
+  // beforeDestroy() {
+  //   clearInterval(this.interval);
+  // },
 };
 </script>
 <style></style>
