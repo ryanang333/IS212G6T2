e9c011438eceba9a65cd8d10a072992fb5e647c8
ryankzl <ryankzl001@gmail.com>
2024-10-27 22:40:37 +0800

    Debug

--- backend/api/utils/creationUtils.js
+++ backend/api/utils/creationUtils.js
@@ -37,17 +37,19 @@ export const createNewCEORequests = async (arrangementRequests, staffId, manager
         }))
       );
   
-      if (reqArr.length > 0) {
-        try {
-          await createAuditEntry(
-            reqArr,
-            staffId,
-            REQUEST_STATUS_NONE,
-            REQUEST_STATUS_APPROVED
-          );
-        } catch (auditError) {
-        };
-    };
+      // if (updatedRequests.modifiedCount > 0) {
+      //   for (const request of requests) {
+      //     try {
+      //       await createAuditEntry(
+      //         previousRequests,
+      //         request.staff_id,
+      //         REQUEST_STATUS_APPROVED,
+      //         REQUEST_STATUS_PENDING_WITHDRAWAL
+      //       );
+      //     } catch (auditError) {
+      //     };
+      //   };
+      // } 
       
     } catch (error) {
       const msg = error.message.includes("Cannot apply - CEO")
@@ -88,17 +90,19 @@ export const createNewRequests = async (arrangementRequests, staffId, managerId)
         }))
       );
   
-      if (reqArr.length > 0) {
-          try {
-            await createAuditEntry(
-              reqArr,
-              staffId,
-              REQUEST_STATUS_NONE,
-              REQUEST_STATUS_PENDING
-            );
-          } catch (auditError) {
-          };
-      };
+      // if (updatedRequests.modifiedCount > 0) {
+      //   for (const request of requests) {
+      //     try {
+      //       await createAuditEntry(
+      //         previousRequests,
+      //         request.staff_id,
+      //         REQUEST_STATUS_APPROVED,
+      //         REQUEST_STATUS_PENDING_WITHDRAWAL
+      //       );
+      //     } catch (auditError) {
+      //     };
+      //   };
+      // } 
     } catch (error) {
       const msg = error.message.includes("Cannot apply")
         ? error.message
